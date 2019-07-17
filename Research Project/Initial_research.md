d0d22e7af978040d9cd9e55fe1b04150be20f5ca
josniff <josniff41@gmail.com>
2019-07-17 09:13:28 +0100

    Buttons working

---
+++ Research Project/Initial_research.md
@@ -0,0 +1,17 @@
+# Research Project 2019
+
+> 04/07/2019 - DD/MM/YYYY
+
+## Outline the Strengths and Weaknesses of the main types of virtualisation. Critically evaluate benefits of virtualisation in the cloud environment
+
+---
+
+Strengths:
+
+- Cost effective
+- Powerful
+- More secure
+- Profitable
+- Increased IT productivity, efficiency, agility and responsivness
+- Configerable
+- Availability of a true software-defined data center
--- Web_dev/CW/script.js
+++ Web_dev/CW/script.js
@@ -4,3 +4,69 @@ function lineRepeat(line, count) {
         line.parentNode.insertBefore(copy, line);
     }
 }
+
+b = 1
+i = 1
+u = 1
+l = 1
+
+function makeBold () {
+    b += 1
+    if (b % 2 == 0) {
+        document.getElementById("button1").style.color = 'black';
+        console.log('Bold')
+    } else if (b % 2 == Math.round(b % 2)) {
+        console.log('Non-bold')
+        document.getElementById("button1").style.color = 'white';
+    }
+    document.execCommand("bold",false,null)
+}
+
+function makeItalic () {
+    i += 1
+    if (i % 2 == 0) {
+        document.getElementById("button2").style.color = 'black';
+        console.log('Italic')
+    } else if (i % 2 == Math.round(i % 2)) {
+        console.log('Non-italic')
+        document.getElementById("button2").style.color = 'white';
+    }
+    document.execCommand("italic",false,null)
+}
+
+function makeUnderline () {
+    u += 1
+    if (u % 2 == 0) {
+        document.getElementById("button3").style.color = 'black';
+        console.log('Underline')
+    } else if (u % 2 == Math.round(u % 2)) {
+        console.log('No-underline')
+        document.getElementById("button3").style.color = 'white';
+    }
+    document.execCommand("underline",false,null)
+}
+
+function makeList () {
+    l += 1
+    if (l % 2 == 0) {
+        document.getElementById("button4").style.color = 'black';
+        console.log('List')
+    } else if (l % 2 == Math.round(l % 2)) {
+        console.log('No-list')
+        document.getElementById("button4").style.color = 'white';
+    }
+    document.execCommand("insertUnorderedList",false,null)
+}
+
+function clone() {
+    console.log("Hello")
+    var itm = document.getElementById("cloneNode");
+    var cln = itm.cloneNode(true);
+
+    document.getElementById("newPad").appendChild(cln);
+}
+
+function deleteNote(textArea)
+{
+    document.getElementByClassName("textArea").innerHTML = "";
+}
