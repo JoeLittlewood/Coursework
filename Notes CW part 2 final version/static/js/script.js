3a0ffd44f4b9c895744c180c02cfc9cddb6a9683
josniff <josniff41@gmail.com>
2019-07-24 12:24:12 +0100

    Commented JS and fixed bugs

--- Notes CW part 2 final version/static/js/script.js
+++ Notes CW part 2 final version/static/js/script.js
@@ -1,97 +1,128 @@
+// ====================================================================================================================
+// SET08702 2018-9 TR2 001 - Web Technologies
+// Joe L - 40417692
+// Coursework Part 2 submission.
+// Javascript document to run SQL database for my Notes application.
+// ====================================================================================================================
+
+// This is a function that clones the horizontal line for an aesthetic look. I have tried to replicate a lined-paper effect.
 function lineRepeat(line, count) {
     for (var i = 0, copy; i < count - 1; i++) {
-        copy = line.cloneNode(); // Clones the horizontal lines
+        copy = line.cloneNode();
         line.parentNode.insertBefore(copy, line);
     }
 }
 
+// Calls the lineRepeat function.
 lineRepeat(document.querySelector('.horizontalLine'), 60, true);
 
+// Used to toggle the button to be black or white when turned on or off.
 b = 1
 
+// Each of the next four functions work very similarly. They all simply toggle the color of the button once pressed and insert a bold, italic or
+// underline tag in the text that the user has written in. This does not work perfectly as the colour of the button can toggle even if the tag has
+// not been pasted into the text.
 function makeBold() {
     b += 1
+
+    // If b is able to be divided by 2 then it is even, therefore the button should turn black.
     if (b % 2 == 0) {
         document.getElementById("button1").style.color = 'black';
-        console.log('Bold')
+        document.execCommand("bold", false, null);
+        // If b is not able to be fully divided by 2 then b is odd, theroefore the button should be white.
     } else if (b % 2 == Math.round(b % 2)) {
-        console.log('Non-bold')
         document.getElementById("button1").style.color = 'white';
     }
-    document.execCommand("bold", false, null)
 }
 
+// Used to toggle the button to be black or white when turned on or off.
 i = 1
 
 function makeItalic() {
     i += 1
     if (i % 2 == 0) {
         document.getElementById("button2").style.color = 'black';
-        console.log('Italic')
+        document.execCommand("italic", false, null);
     } else if (i % 2 == Math.round(i % 2)) {
-        console.log('Non-italic')
         document.getElementById("button2").style.color = 'white';
     }
-    document.execCommand("italic", false, null)
 }
 
+// Used to toggle the button to be black or white when turned on or off.
 u = 1
 
 function makeUnderline() {
     u += 1
     if (u % 2 == 0) {
         document.getElementById("button3").style.color = 'black';
-        console.log('Underline')
+        document.execCommand("underline", false, null);
     } else if (u % 2 == Math.round(u % 2)) {
-        console.log('No-underline')
         document.getElementById("button3").style.color = 'white';
     }
-    document.execCommand("underline", false, null)
 }
 
+// Used to toggle the button to be black or white when turned on or off.
 l = 1
 
 function makeList() {
     l += 1
     if (l % 2 == 0) {
         document.getElementById("button4").style.color = 'black';
-        console.log('List')
+        document.execCommand("insertUnorderedList", false, null);
     } else if (l % 2 == Math.round(l % 2)) {
-        console.log('No-list')
         document.getElementById("button4").style.color = 'white';
     }
-    document.execCommand("insertUnorderedList", false, null)
 }
 
-y = 0
+y = 1
 
+// This function clones the "pad" div and shows it with a unique ID ready to be saved by the user.
 function clone() {
-    y += 1;
+
+    var itm = document.getElementById("cloneNode");
+    var help = document.getElementById("help");
+    var cln = itm.cloneNode(true);
+
+    // Hides all notes so that only the active note can be shown later.
     $('#newPad').children('div').each(function() {
         $(this).css("visibility", "hidden");
     })
 
+    // Appends the title of the new note to the dropdown menu.
     $("#dropdownContent").append("<a onclick='showNote(\"newNote" + y + "\")'>newNote</a>");
-    var itm = document.getElementById("cloneNode");
-    var help = document.getElementById("help");
-    var cln = itm.cloneNode(true);
+
+    // Changes the id of the new note to "newNote(y)".
     cln.id = "newNote" + y;
 
+    // Appends the new note to the newPad div, adds a title to it and then makes it visible using the showNote function.
     document.getElementById("newPad").appendChild(cln);
     var newItm = document.getElementById("newNote" + (y));
     $("#newNote" + y).attr("title", "");
     showNote("newNote" + y);
+
+    // y is incrimented to give each note a unique id.
+    y += 1;
 }
 
+// This function will ask if you are sure you would like to delete the note, then the note will be removed from the database.
 function delNotes() {
+
+    // Makes sure user has not made an error by clicking the bin.
     var x = confirm("Are you sure you want to delete this note?");
+
     if (x == true) {
+
         idToDelete = -1;
+
+        // Finds which note is active by finding which one has the visibility attribute set to "visible" and then gets it's id.
         $('#newPad').children('div').each(function() {
             if ($(this).css("visibility") == "visible") {
                 idToDelete = $(this).attr("id");
             }
         })
+
+        // a.jax is used to post the information to the database. This is then picked up in the app.js file where the item
+        // is removed from the database.
         $.ajax({
             url: "/del-note",
             type: "POST",
@@ -103,75 +134,94 @@ function delNotes() {
                 displayNotes();
             }
         })
+
+        // Becasue the title is separate from the pad, this is then cleared.
         $("#title").html("");
-    } else {
-        txt = "Canceled."
+
+        // Shows help message when all notes are hidden.
+        if ($("#newPad").html().length > 1) {
+            $("#help").css("visibility", "visible");
+        }
     }
-    console.log(y);
 }
 
+// This function will display the active note.
 function displayNotes(noteID) {
+
     $.ajax({
         url: "/get-notes",
         type: "GET",
         success: function(result) {
-            $("#newPad").html("");
-            $("#dropdownContent").html("");
+
+            $("#newPad").html(""); // The newPad div is cleared first to remove any deleted notes.
+            $("#dropdownContent").html(""); // The dropdown content is cleared also to remove any deleted notes.
+
             result.forEach(Element => {
                 var noteID = Element.noteid;
                 var title = Element.title;
                 var content = Element.content;
 
-                var html = '<div class="pad" id="' + noteID + '" title="' + title + '">' +
-                    '<div class="verticalLine"></div>' +
-                    '<div class="spacer"></div>' +
-                    '<div class="textArea" contenteditable="true" placeholder="Write your note here!" id="' + noteID + '"' +
-                    'spellcheck="true">' + content + '</div>' +
-                    '<div id="lineclone' + noteID + '" class="horizontalLine"></div>' +
-                    '</div>'
+                var html = '<div class="pad" id="' + noteID + '" title="' + title + '"><div class="verticalLine"></div><div class="spacer"></div>' +
+                    '<div class="textArea" contenteditable="true" placeholder="Write your note here!" id="' + noteID + '"spellcheck="true">' + content +
+                    '</div><div id="lineclone' + noteID + '" class="horizontalLine"></div></div>'
 
+                // The new html that is written above is appended to the newPad div, which remains hidden until showNote() is ran.
                 $("#newPad").append(html);
                 lineRepeat(document.querySelector('#lineclone' + noteID), 60, true);
-
-                $("#dropdownContent").append("<a onclick='showNote(" + noteID + ")'>" + title + "</a>");
-                $("#title").html("");
+                $("#dropdownContent").append("<a onclick='showNote(" + noteID + ")'>" + title + "</a>"); // Titles are appended to the dropdown content menu.
+                $("#title").html(""); // The title is cleared as this is separate from the pads.
             })
         }
     })
 }
 
+// When a note is shown, all other notes are hidden and the note that has been selected is set to visible.
 function showNote(id) {
+
+    // The help message is hidden as it is no longer needed.
     $("#help").css("visibility", "hidden");
+
+    // All notes are hidden.
     $('#newPad').children('div').each(function() {
         $(this).css("visibility", "hidden");
     })
 
+    // The note that has been selected is set to visible.
     $("#" + id).css("visibility", "visible");
     $("#title").html($("#" + id).attr("title"));
 }
 
+// This function saves the selected note to the database.
 function saveNote() {
+
     var idToSave = -1;
+
+    // The id is taken from the note that is selected.
     $('#newPad').children('div').each(function() {
         if ($(this).css("visibility") == "visible") {
             idToSave = $(this).attr("id");
         }
     })
-    if ($.isNumeric(idToSave)) {
-        $.ajax({
-            url: "/edit-note",
-            type: "POST",
-            data: {
-                noteid: idToSave,
-                title: $("#title").html(),
-                content: $("#" + idToSave).children('.textArea').html()
-            },
 
-            success: function(result) {
-                displayNotes();
-            }
-        })
+    // If the id is only numeric, this means it is an old note that has previously been saved and will need to be updated.
+    if ($.isNumeric(idToSave)) {
+        // a.jax sends the data to app.js to update the current note.
+        $.ajax({
+                url: "/edit-note",
+                type: "POST",
+                data: {
+                    noteid: idToSave,
+                    title: $("#title").html(),
+                    content: $("#" + idToSave).children('.textArea').html()
+                },
+
+                success: function(result) {
+                    displayNotes();
+                }
+            })
+            // If the id of the note is not all numeric, then it is a new note and will need to be added to the database.
     } else {
+        // a.jax sends the data to app.js to create a new note.
         $.ajax({
             url: "/new-note",
             type: "POST",
@@ -187,27 +237,36 @@ function saveNote() {
     }
 }
 
+// This function calls the downloadInnerHtml function that downloads the note as a html file. This preserves the formatting.
 function downloadNote(idToDownload, filename) {
+
+    // Downloaded filename is changed to title of current note.
+    var filename = $("#title").html() + '.html';
     idToDownload = -1;
+
+    // As before, the id is set for the current active note.
     $('#newPad').children('div').each(function() {
         if ($(this).css("visibility") == "visible") {
             idToDownload = $(this).attr("id");
         }
     })
-    var filename = $("#title").html() + '.html';
+
+
     if (idToDownload != -1) {
+        // Download function is called.
         downloadInnerHtml(filename, idToDownload, "text/html");
     }
 }
 
+// This function downloads the file.
 function downloadInnerHtml(filename, idToDownload, mimeType) {
-    var elHtml = document.getElementById(idToDownload).innerHTML;
-    var link = document.createElement('a');
-    mimeType = mimeType || 'text/plain';
+    var text = document.getElementById(idToDownload).innerHTML;
+    var doc = document.createElement('a');
+    mimeType = mimeType || 'text/plain'; // Multi-purpose Internet Mail Extensions type. Saves as plain text.
 
-    link.setAttribute('download', filename);
-    link.setAttribute('href', 'data:' + mimeType + ';charset=utf-8,' + encodeURIComponent(elHtml));
-    link.click();
+    doc.setAttribute('download', filename);
+    doc.setAttribute('href', 'data:' + mimeType + ';charset=utf-8,' + encodeURIComponent(text));
+    doc.click();
 }
 
 displayNotes();
